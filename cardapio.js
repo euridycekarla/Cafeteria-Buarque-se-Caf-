f1141033093979d09bf59c0822a5552a8d2a7387
Euridyce Karla Carvalho <91157048+euridycekarla@users.noreply.github.com>
2022-02-20 23:52:34 -0300

    Add files via upload

---
+++ cardapio.js
@@ -0,0 +1,54 @@
+import itens from './model/dataset.js';
+import foodsModel from './model/foods.js';
+
+foodsModel.load(itens);
+let foods = foodsModel.readAll();
+
+function initFoodsCard () {
+  
+  for (let item of foods) {
+
+    const view = createFoodCardItem(item);
+  
+    //let itensCardapio = document.querySelector('.itens-cardapio');
+    let itensCardapio = document.getElementById("itens-cardapio");
+    itensCardapio.insertAdjacentHTML('beforeend', view);
+  }
+}
+
+function createFoodCardItem (item) {
+
+    const view = `<div class="col-3 card my-1 mx-1 py-1">
+                    <img src="${item.image}" class="card-img-top" alt="...">
+  
+                    <div class="card-body">
+                      <h5 class="card-title">${item.name}</h5>
+                      <p class="card-text">${item.description}</p>
+                      <h5 class="card-text">${item.price}</h5>
+                      <a href="#" class="btn btn-primary">Comprar</a>
+                    </div>
+                  </div>`;
+
+    return view;
+}
+
+
+// Captar o evento de submissão do formulário e adicionar o item no cartão (card).
+// const foodForm = document.querySelector('#foodForm');
+const foodForm = document.getElementById("foodForm");
+
+foodForm.onsubmit = function (event) {
+  // Previnir que o modal fique abrindo e fechando em loop.
+  event.preventDefault();
+
+  let newFood = Object.fromEntries(new FormData(foodForm));
+  foodsModel.create(newFood);
+
+  const foodCard = createFoodCardItem(newFood);
+  let itensCardapio = document.getElementById("itens-cardapio");
+  itensCardapio.insertAdjacentHTML('beforeend', foodCard);
+}
+
+initFoodsCard();
+
+const limpar = document.getElementById("limparformulario");
---
+++ dataset.js
@@ -0,0 +1,36 @@
+const dataset = [
+  {
+    id: 1,
+    name: 'Café',
+    image: 'imagem/cafe.jpg',
+    description:
+      'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína.',
+      price:'R$5,00'
+  },
+  {
+    id: 2,
+    name: 'Croissant',
+    image: 'imagem/croissant.jpg',
+    description:
+      'Um croissant aportuguesado como croassã ou croassão, é um bolo de massa folhada em formato de meia-lua, feito de farinha, açúcar, sal, leite, fermento, manteiga e ovo para pincelar.Tem origem na Viena,Áustria.',
+      price:'R$8,00'
+  },
+  {
+    id: 3,
+    name: 'Pastel',
+    image: 'imagem/pastel.jpg',
+    description:
+      'Pastel é um alimento composto por uma massa à base de farinha a que se dá a forma de um envelope, se recheia e depois se frita por imersão em óleo fervente ou  é assado no forno. Originário da culinária europeia, o pastel um dos alimentos mais frequentemente encontrados em carrinhos de rua e centros de comércio popular do Brasil e Portugal, também populares em outros países.',
+      price:'R$8,50'
+  },
+  {
+    id: 4,
+    name: 'Cupcake',
+    image: 'imagem/bolo.jpg',
+    description:
+      'É um pequeno bolo designado para servir uma única pessoa,geralmente possui cobertura.',
+      price:'R$5,00'
+  },
+];
+
+export default dataset;
